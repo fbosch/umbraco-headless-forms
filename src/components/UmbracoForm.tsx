f8c1dccaece318b03a3c090f8d277ba3e1052ca8
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2024-06-01 17:17:11 +0200

    feat: custom rendering validation

--- src/components/UmbracoForm.tsx
+++ src/components/UmbracoForm.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react";
+import React, { Fragment, useId } from "react";
 import type {
   UmbracoFormDefinition,
   FormPageDto,
@@ -10,30 +10,37 @@ import type {
 interface CommonRenderProps {
   children: React.ReactNode;
 }
+type RenderProps = CommonRenderProps &
+  (
+    | {
+        page: FormPageDto;
+      }
+    | {
+        fieldset: FormFieldsetDto;
+      }
+    | {
+        column: FormFieldsetColumnDto;
+      }
+    | {
+        field: FormFieldDto;
+      }
+  );
 
-interface RenderPageProps extends CommonRenderProps {
-  page: FormPageDto;
-}
+type RenderPageProps = RenderProps & { page: FormPageDto };
 
-interface RenderFieldsetProps extends CommonRenderProps {
-  fieldset: FormFieldsetDto;
-}
+type RenderFieldsetProps = RenderProps & { fieldset: FormFieldsetDto };
 
-interface RenderColumnProps extends CommonRenderProps {
-  column: FormFieldsetColumnDto;
-}
+type RenderColumnProps = RenderProps & { column: FormFieldsetColumnDto };
 
-interface RenderFieldProps extends CommonRenderProps {
+type RenderFieldProps = RenderProps & { field: FormFieldDto };
+
+type RenderInputProps = Omit<CommonRenderProps, "children"> & {
   field: FormFieldDto;
-}
+};
 
-interface RenderInputProps extends Omit<CommonRenderProps, "children"> {
-  field: FormFieldDto;
-}
-
-interface RenderSubmitButtonProps {
+type RenderSubmitButtonProps = {
   form: UmbracoFormDefinition;
-}
+};
 
 export interface UmbracoFormProps
   extends React.FormHTMLAttributes<HTMLFormElement> {
@@ -43,7 +50,7 @@ export interface UmbracoFormProps
   renderFieldset?: (props: RenderFieldsetProps) => React.ReactNode;
   renderColumn?: (props: RenderColumnProps) => React.ReactNode;
   renderField?: (props: RenderFieldProps) => React.ReactNode;
-  renderInput?: (props: RenderInputProps) => React.ReactNode;
+  renderInput?: (props: RenderInputProps) => React.ReactNode | undefined;
   renderSubmitButton?: (props: RenderSubmitButtonProps) => React.ReactNode;
 }
 
@@ -53,7 +60,7 @@ function defaultRenderPage({
 }: RenderPageProps): React.ReactNode {
   return (
     <Fragment>
-      <h2>{page.caption}</h2>
+      {page.caption ? <h2>{page.caption}</h2> : null}
       {children}
     </Fragment>
   );
@@ -65,7 +72,7 @@ function defaultRenderFieldset({
 }: RenderFieldsetProps): React.ReactNode {
   return (
     <Fragment>
-      <h3>{fieldset.caption}</h3>
+      {fieldset.caption ? <h3>{fieldset.caption}</h3> : null}
       {children}
     </Fragment>
   );
@@ -77,7 +84,7 @@ function defaultRenderColumn({
 }: RenderColumnProps): React.ReactNode {
   return (
     <Fragment>
-      <h4>{column.caption}</h4>
+      {column.caption ? <h4>{column.caption}</h4> : null}
       {children}
     </Fragment>
   );
@@ -95,7 +102,9 @@ function defaultRenderField({
   );
 }
 
-function mapFieldTypeNameToInput({ field }: RenderInputProps): React.ReactNode {
+function mapFieldTypeNameToInputNode({
+  field,
+}: RenderInputProps): React.ReactNode {
   const common = {
     name: field.alias,
     id: field.alias,
@@ -118,11 +127,32 @@ function mapFieldTypeNameToInput({ field }: RenderInputProps): React.ReactNode {
     case "Checkbox":
       return <input type="checkbox" {...common} />;
     case "Multiple choice":
+      return (
+        <Fragment>
+          {field?.preValues?.map((preValue) => {
+            const id = common.id + "-" + preValue.value;
+            return (
+              <Fragment key={id}>
+                <label htmlFor={id}>{preValue.caption}</label>
+                <input
+                  {...common}
+                  id={id}
+                  type="radio"
+                  value={preValue.value}
+                />
+              </Fragment>
+            );
+          })}
+        </Fragment>
+      );
     case "Dropdown":
       return (
         <select {...common} multiple={field.settings?.allowMultipleSelections}>
-          {field.preValues.map((preValue, index) => (
-            <option key={"preValue-" + index} value={preValue.value}>
+          {field.preValues.map((preValue) => (
+            <option
+              key={common.id + "-" + preValue.value}
+              value={preValue.value}
+            >
               {preValue.caption}
             </option>
           ))}
@@ -147,10 +177,8 @@ function mapFieldTypeNameToInput({ field }: RenderInputProps): React.ReactNode {
   }
 }
 
-function defaultRenderInput({
-  field,
-}: RenderInputProps): React.ReactNode | undefined {
-  return mapFieldTypeNameToInput({ field });
+function defaultRenderInput({ field }: RenderInputProps): React.ReactNode {
+  return mapFieldTypeNameToInputNode({ field });
 }
 
 function defaultRenderSubmitButton({
@@ -163,7 +191,73 @@ function defaultRenderSubmitButton({
   );
 }
 
+function isDefaultKey(key: React.Key): boolean {
+  return /^\.\d+$/.test(String(key));
+}
+
+function recursiveGetChildKeys(element: React.ReactElement): string[] {
+  const keys: string[] = [];
+
+  if (element.props.children) {
+    const children = React.Children.toArray(element.props.children);
+    children.forEach((child) => {
+      if (React.isValidElement(child) && child.key) {
+        if (child.key && !isDefaultKey(child.key)) {
+          keys.push(String(child.key));
+        }
+        keys.push(...recursiveGetChildKeys(child));
+      }
+    });
+  }
+
+  return keys.filter(Boolean).map((key) => key.split("$")[1]);
+}
+
+function renderAndAssertChildren<T extends RenderProps>(
+  renderFn: (props: T) => React.ReactNode,
+) {
+  return (props: Omit<T, "children">) => (children: React.ReactNode) => {
+    const output = renderFn({ ...props, children } as T) as React.ReactElement;
+    const childKeys = recursiveGetChildKeys({
+      props: { children },
+    } as React.ReactElement);
+    // get all the keys from the render output
+    const keys = recursiveGetChildKeys(output);
+    // check that all the keys from the children are present in the output
+    if (!childKeys.every((key) => keys.includes(key))) {
+      // if not, throw an error
+      throw new Error(
+        `(${renderFn.name}) required children from props are not present in the render output`,
+      );
+    }
+    return output;
+  };
+}
+
+/**
+ * Creates a render function with optional custom rendering capability. If customRenderer matches defaultRenderer,
+ * it returns a function passing children through defaultRenderer. Otherwise, it validates children presence in the output of the customRenderer.
+ *
+ * @param {Function} defaultRenderer - Default rendering function
+ * @param {Function} customerRenderer - Custom rendering function
+ * @returns {Function} renderFunction
+ */
+function createRenderFunction<T extends RenderProps>(
+  defaultRenderer: (props: T) => React.ReactNode,
+  customRenderer: (props: T) => React.ReactNode,
+): (
+  props: Omit<T, "children">,
+) => (children: React.ReactNode) => React.ReactNode {
+  if (customRenderer === defaultRenderer) {
+    return (props: Omit<T, "children">) => (children: React.ReactNode) =>
+      defaultRenderer({ ...props, children } as T);
+  }
+  // assert that the children of the props are present in the rendered output if custom rendering is used
+  return renderAndAssertChildren<T>(customRenderer);
+}
+
 function UmbracoForm(props: UmbracoFormProps) {
+  const id = useId();
   const {
     form,
     onSubmit,
@@ -173,45 +267,49 @@ function UmbracoForm(props: UmbracoFormProps) {
     renderField = defaultRenderField,
     renderInput = defaultRenderInput,
     renderSubmitButton = defaultRenderSubmitButton,
+    children,
     ...rest
   } = props;
+
+  const _renderPage = createRenderFunction(defaultRenderPage, renderPage);
+
+  const _renderFieldset = createRenderFunction(
+    defaultRenderFieldset,
+    renderFieldset,
+  );
+  const _renderColumn = createRenderFunction(defaultRenderColumn, renderColumn);
+
+  const _renderField = createRenderFunction(defaultRenderField, renderField);
 
   return (
     <form {...rest} id={"form-" + form.id} name={form.id}>
       {form.pages.map((page, index) => (
-        <Fragment key={"page-" + index}>
-          {renderPage({
-            page,
-            children: page.fieldsets.map((fieldset, index) => (
-              <Fragment key={"fieldset-" + index}>
-                {renderFieldset({
-                  fieldset,
-                  children: fieldset.columns.map((column, index) => (
+        <Fragment key={id + "-page-" + index}>
+          {_renderPage({ page })(
+            page.fieldsets.map((fieldset, index) => (
+              <Fragment key={id + "-fieldset-" + index}>
+                {_renderFieldset({ fieldset })(
+                  fieldset.columns.map((column, index) => (
                     <Fragment key={"column-" + index}>
-                      {renderColumn({
-                        column,
-                        children: column.fields.map((field, index) => (
-                          <Fragment key={"field-" + index}>
-                            {renderField({
-                              field,
-                              children:
-                                renderInput({
-                                  field,
-                                }) ??
-                                // fallback to default input rendering if custom rendering returns undefined
+                      {_renderColumn({ column })(
+                        column.fields.map((field, index) => (
+                          <Fragment key={id + "-field-" + index}>
+                            {_renderField({ field })(
+                              renderInput({ field }) ??
                                 defaultRenderInput({ field }),
-                            })}
+                            )}
                           </Fragment>
                         )),
-                      })}
+                      )}
                     </Fragment>
                   )),
-                })}
+                )}
               </Fragment>
             )),
-          })}
+          )}
         </Fragment>
       ))}
+      {children}
       {renderSubmitButton({ form })}
     </form>
   );
