d285ff20a706cf85458ae5174827f9d03dcab3ab
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2024-06-08 14:06:07 +0200

    feat: added enum for field ids

--- src/components/types.ts
+++ src/components/types.ts
@@ -23,6 +23,26 @@ const DEFAULT_FORM_FIELD_TYPE_NAMES = [
   // "Single choice",
 ] as const;
 
+/** Enumeration of default form field type ids
+ * * @see https://docs.umbraco.com/umbraco-forms/editor/creating-a-form/field-types */
+export enum FormFieldTypeIdEnum {
+  Checkbox = "d5c0c390-ae9a-11de-a69e-666455d89593",
+  DataConsent = "a72c9df9-3847-47cf-afb8-b86773fd12cd",
+  Date = "f8b4c3b8-af28-11de-9dd8-ef5956d89593",
+  DropdownList = "0dd29d42-a6a5-11de-a2f2-222256d89593",
+  FileUpload = "84a17cf8-b711-46a6-9840-0e4a072ad000",
+  LongAnswer = "023f09ac-1445-4bcb-b8fa-ab49f33bd046",
+  HiddenField = "da206cae-1c52-434e-b21a-4a7c198af877",
+  MultipleChoice = "fab43f20-a6bf-11de-a28f-9b5755d89593",
+  Password = "fb37bc60-d41e-11de-aeae-37c155d89593",
+  Recaptcha2 = "fb37bc60-d41e-11de-aeae-37c155d89593",
+  RecaptchaV3WithScore = "663aa19b-423d-4f38-a1d6-c840c926ef86",
+  RichText = "1f8d45f8-76e6-4550-a0f5-9637b8454619",
+  SingleChoice = "903df9b0-a78c-11de-9fc1-db7a56d89593",
+  ShortAnswer = "3f92e01b-29e2-4a30-bf33-9df5580ed52c",
+  TitleAndDescription = "e3fbf6c4-f46c-495e-aff8-4b3c227b4a98",
+}
+
 /** Default form field type name
  * @see https://docs.umbraco.com/umbraco-forms/editor/creating-a-form/field-types */
 export type DefaultFormFieldTypeName =
@@ -152,29 +172,43 @@ export interface UmbracoFormFieldSettingsMap {
     errorMessage: string;
     saveScore: string;
   };
+  Date: {
+    placeholder: string;
+  };
+  Password: {
+    placeholder: string;
+  };
+  RichText: {
+    showLabel: string;
+    html: string;
+  };
+  "Single choice": {
+    defaultValue: string;
+    showLabel: string;
+  };
 }
 
-/** configuration for UmbracoForm component instance */
+/** Configuration for the UmbracoForm component instance */
 export interface UmbracoFormConfig {
-  /** custom schema for form validation, defaults to umbracoFormToZod implementation */
+  /** Custom schema for form validation; defaults to the umbracoFormToZod implementation */
   schema: ReturnType<typeof umbracoFormToZod>;
-  /** optional custom mapping function for form fields, useful when you want to handle custom field types */
+  /** Optional custom function to map form fields to Zod types, useful for handling custom field types */
   mapCustomFieldToZodType?: MapFormFieldToZod;
-  /** whether or not client side validation should be used, defaults to `false` */
+  /** Flag indicating if client-side validation should be performed; defaults to `false` */
   shouldValidate?: boolean;
-  /** whether or not native browser validation should be used in combination with client side validation, defaults to `false` */
+  /** Flag indicating if native browser validation should be used alongside client-side validation; defaults to `false` */
   shouldUseNativeValidation?: boolean;
 }
 
 export interface UmbracoFormContext {
-  /** form definition */
+  /** The definition of the form */
   form: FormDto;
-  /** configuration for form component */
+  /** The configuration for the form component */
   config: UmbracoFormConfig;
-  /** number of times the form or a form page withing the form has been attempted to be submitted */
+  /** The number of times the form or a form page within the form has been attempted to be submitted */
   progressionAttemptCount: number;
-  /** current page number */
+  /** The current page number of the form */
   currentPage: number;
-  /** total number of pages */
+  /** The total number of pages in the form */
   totalPages: number;
 }
